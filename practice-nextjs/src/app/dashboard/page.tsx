abfafa9cd1c81323de7b428f15f39d9a38bbb003
popeye1235 <popeyemishra@gmail.com>
2025-05-21 23:34:27 +0530

    file colocation

---
+++ practice-nextjs/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+//  function BarChart() {
+//     return (
+//       <>
+//         <p className="font-bold text-cyan-500">bar chart!</p>
+//       </>
+//     );
+//   }
+
+  export default function Dashboard() {
+    return <h1>Dashboard!</h1>
+  }
+  
